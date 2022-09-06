4cbd32a7d82903313da052810d98e594f3de2030
David Olžbut <david.olzbut@sps-prosek.cz>
2022-09-06 15:00:01 +0200

    Přidejte soubory projektu.

---
+++ VA_2022_3ITA_S1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VA_2022_3ITA_S1
+{
+    internal class Class1
+    {
+
+
+    }
+}
